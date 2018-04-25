fa82385354b8f4f0bf838ce10831c7e4806133a4
Pradyuth1 <pvangur@purdue.edu>
2018-04-25 18:23:21 -0400

    Update
    Adding the distance between various points.

--- Project.R
+++ Project.R
@@ -1,7 +1,44 @@
-Training_data <- function(a){
-  Iris <- read.csv("C:\\Users\\pradyuth\\STAT 598 project\\Iris.csv")
+library(plyr)
+Iris <- read.csv("C:\\Users\\pradyuth\\STAT 598 project\\Iris.csv")
+
+Split_data <- function(a){
   u <- nrow(Iris)
   Iris_training <- Iris[1:(a*u), ]
-  Iris_test <- Iris[1:((1-a)*u), ]
+  Iris_test <- Iris[((a*u)+1):(u+1), ]
   return(list(Iris_training, Iris_test))
 }
+
+set_1 <- Split_data(0.85)
+set_2 <- Split_data(0.70)
+
+nrow(Iris)
+
+euc_dist<- function(p,q){
+  len<- sqrt(sum((p-q)^2))
+  return(len)
+}
+
+distance <- matrix(0, nrow = nrow(set_1[[1]]), ncol = nrow(set_1[[2]]))
+
+for(i in 1:nrow(set_1[[2]])){
+  for(j in 1:nrow(set_1[[1]])){
+    distance[j, i] <- euc_dist(set_1[[1]][j, 2:5], set_1[[2]][i, 2:5])
+  }
+}
+
+#Find k least distances
+indices <- sort(distance[, 2], decreasing = T, index.return = T)[[2]][1:4]
+
+#Returning the categorical variable for indices obtained
+set_1[[1]][indices, 6]
+
+
+
+
+
+
+
+
+
+
+
